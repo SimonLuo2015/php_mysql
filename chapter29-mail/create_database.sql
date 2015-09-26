b9642def51be178884d5449612bdba5412835572
zhlinh <zhlinhng@gmail.com>
2015-09-26 12:30:50 +0800

    add cahpter29-mail

---
+++ chapter29-mail/create_database.sql
@@ -0,0 +1,23 @@
+create database mail;
+use mail;
+
+create table users (
+    username char(16) not null primary key,
+    password char(40) not null,
+    address char(100) not null,
+    displayname char(100) not null
+) engine=InnoDB;
+
+create table accounts (
+    username char(16) not null,
+    server char(100) not null,
+    port int not null,
+    type char(4) not null,
+    remoteuser char(50) not null,
+    remotepassword char(50) not null,
+    accountid int unsigned not null auto_increment primary key
+) engine=InnoDB;
+
+grant select, insert, update, delete
+on mail.*
+to mail@localhost identified by 'password';
